d2235d1fd2c0fb527fb72441fbea575d424ea14e
Mahudoom <66937573+easa2000@users.noreply.github.com>
2021-08-31 21:54:21 +0530

    Create enumerrated data types.c

---
+++ enumerrated data types.c
@@ -0,0 +1,14 @@
+// enumerated data types
+#include<stdio.h>
+int main()
+{
+    enum {RED=2, BLUE, BLACK = 5, GREEN = 7, YELLOW , PURPLE ,WHITE = 15};
+    printf("\n RED = %d",RED);
+    printf("\n BLUE = %d",BLUE);
+    printf("\n BLACK = %d",BLACK);
+    printf("\n GREEN = %d",GREEN);
+    printf("\n YELLOW = %d",YELLOW);
+    printf("\n PURPLE = %d",PURPLE);
+    printf("\n WHITE = %d",WHITE);
+    
+}
